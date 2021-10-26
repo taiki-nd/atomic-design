c2dc8011af24e43fc2eadfafaff4dfc395f85355
Taiki Noda <84260062+taiki-nd@users.noreply.github.com>
2021-10-26 15:29:40 +0900

    molecuresの作成

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { SecondaryButton } from "./components/atoms/button/SecoondaryButton";
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import "./styles.css";
+import { SerachInput } from "./components/molecures/SearchInput";
 
 export default function App() {
   return (
     <div className="App">
       <PrimaryButton>test</PrimaryButton>
       <SecondaryButton>検索</SecondaryButton>
+      <br />
+      <SerachInput />
     </div>
   );
 }
---
+++ src/components/molecures/SearchInput.jsx
@@ -0,0 +1,22 @@
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
+import { Input } from "../atoms/input/Input";
+import styled from "styled-components";
+
+export const SerachInput = () => {
+  return (
+    <SContainer>
+      <Input placeholder="検索条件を入力" />
+      <SButtonWrapper />
+      <PrimaryButton>検索</PrimaryButton>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const SButtonWrapper = styled.div`
+  padding-left: 8px;
+`;
